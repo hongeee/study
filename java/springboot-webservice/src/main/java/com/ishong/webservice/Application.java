f867922a3965d5e02dfb2e96da55947ac159be1d
87ishong <87ishong@naver.com>
2019-03-11 20:17:01 +0900

    Package명 수정 및 Application 클래스 이름 변경

--- java/springboot-webservice/src/main/java/com/ishong/webservice/Application.java
+++ java/springboot-webservice/src/main/java/com/ishong/webservice/Application.java
@@ -1,13 +1,13 @@
-package com.ishong.springbootwebservice;
+package com.ishong.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootWebserviceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootWebserviceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
